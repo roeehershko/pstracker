2166e7cf1a0371b2d244fa06e0239ff4a87e1da8
royh <royh@affilomania.com>
2017-12-31 16:51:45 +0200

    te

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
-// Include the cluster module
-var cluster = require('cluster');
-// Code to run if we're in the master process
-if (cluster.isMaster) {
-    // Count the machine's CPUs
-    var cpuCount = require('os').cpus().length;
+var visits = 1;
+// Include Express
+var express = require('express');
+var fs = require('fs');
 
-    // Create a worker for each CPU
-    for (var i = 0; i < 1; i += 1) {
-        cluster.fork();
-    }
-// Code to run if we're in a worker process
-} else {
-    var visits = 1;
-    // Include Express
-    var express = require('express');
-    var fs = require('fs');
+// Create a new Express application
+var app = express();
 
-    // Create a new Express application
-    var app = express();
+// Add a basic route – index page
+app.get('/', function (req, res) {
+    res.redirect('http://www.walla.co.il');
+});
 
-    // Add a basic route – index page
-    app.get('/', function (req, res) {
-        res.redirect('http://www.walla.co.il');
-    });
-
-    // Bind to a port
-    app.listen(3000);
-    console.log('Application running #' + cluster.worker.id);
-}
+// Bind to a port
+app.listen(3000);
+console.log('Application running');
