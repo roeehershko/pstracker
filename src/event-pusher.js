302f81e89cedf2fd2275e6d5852674b14b6ed54b
royh <royh@affilomania.com>
2018-01-09 12:46:31 +0200

    te

--- src/event-pusher.js
+++ src/event-pusher.js
@@ -7,20 +7,12 @@ class EventPusher {
 
     constructor() {
         const self = this;
-
+        this.client = null;
         this.peoples = [];
         this.campaigns = [];
         this.eventKeys = [
             'c', 'e', 'ip', 'ua', 's'
         ];
-
-        mongoClient.connect(url, function (err, client) {
-            let campaignsCollection = client.db('pstracker').collection('campaigns');
-            campaignsCollection.find({}).toArray(function (err, data) {
-                self.campaigns = data;
-                client.close();
-            });
-        });
     }
 
     clean() {
@@ -28,11 +20,11 @@ class EventPusher {
         this.sessions = [];
     }
 
-    extractTracking(event) {
+    extractTracking(event, appendPrefix) {
         let tracking = {};
         for (let key in event) {
             if (this.eventKeys.indexOf(key) === -1) {
-                tracking["tracking." + key] = event[key];
+                tracking[( appendPrefix  ? "tracking." : "") + key] = event[key];
             }
         }
         return tracking;
@@ -40,53 +32,86 @@ class EventPusher {
 
     push(events) {
         const self = this;
-        events.forEach(function (event) {
-            const campaign = self.campaigns.find(o => o.name === event.c);
+        let documents = [];
 
-            if (campaign) {
-                // Get campaign event
-                const campaignEvent = event.e
-                    ? campaign.events.find(o => o.name === event.e)
-                    : campaign.events.find(o => o.is_default === true);
+        this.startPushOperation(function () {
+            events.forEach(function (event) {
+                const campaign = self.campaigns.find(o => o.name === event.c);
 
-                // Get campaign source
-                const campaignSource = event.s
-                    ? campaign.sources.find(o => o.name === event.s)
-                    : campaign.sources.find(o => o.is_default === true);
+                if (campaign) {
+                    // Get campaign event
+                    const campaignEvent = event.e
+                        ? campaign.events.find(o => o.name === event.e)
+                        : campaign.events.find(o => o.is_default === true);
 
-                // Create unique identity for session campaign + ip + 4 days max
-                const guid = aguid(campaign.name + '@' + event.ip);
+                    // Get campaign source
+                    const campaignSource = event.s
+                        ? campaign.sources.find(o => o.name === event.s)
+                        : campaign.sources.find(o => o.is_default === true);
 
-                campaignEvent.revenue = parseFloat(event.r) ? event.r : campaignEvent.revenue || 0.0;
-                let session = {
-                    time: new Date(),
-                    source: campaignSource,
-                    ua: event.ua
-                };
+                    // Create unique identity for session campaign + ip + 4 days max
+                    const guid = aguid(campaign.name + '@' + event.ip);
 
-                mongoClient.connect(url, function (err, client) {
-                    let campaignsCollection = client.db('pstracker').collection('peoples');
-                    campaignsCollection.updateOne({
-                            guid: guid
-                        },
-                        {
-                            $set: Object.assign({
-                                guid: guid,
-                                last_seen: new Date(),
-                            }, self.extractTracking(event)),
-                            $push: {
-                                events: campaignEvent,
-                                sessions: session
-                            },
-                            $setOnInsert: {
-                                first_session: session,
-                                source: campaignSource
+                    campaignEvent.revenue = parseFloat(event.r) ? event.r : campaignEvent.revenue || 0.0;
+                    let session = {
+                        time: new Date(),
+                        ip: event.ip,
+                        ua: event.ua,
+                        tracking: self.extractTracking(event, false)
+                    };
+
+                    documents.push({
+                        findO: {guid: guid},
+                        update: {
+                                $set: Object.assign({
+                                    guid: guid,
+                                    last_seen: new Date(),
+                                }, self.extractTracking(event, true)),
+                                $push: {
+                                    events: campaignEvent,
+                                    sessions: session
+                                },
+                                $setOnInsert: {
+                                    first_session: session,
+                                    source: campaignSource,
+                                    campaign: campaign._id
+                                }
                             }
-                        },
-                        {upsert: true})
-                });
-            }
-        })
+                    });
+                }
+            });
+
+            this.insertBulkEventsDocuments(documents, function () {
+                self.client.close();
+            });
+        });
+    }
+
+    startPushOperation(cb) {
+        const self = this;
+        mongoClient.connect(url, function (err, client) {
+            let campaignsCollection = client.db('pstracker').collection('campaigns');
+            campaignsCollection.find({}).toArray(function (err, data) {
+                self.campaigns = data;
+                self.client = client;
+                cb.call(self);
+            });
+        });
+    }
+
+    insertBulkEventsDocuments(documents, cb) {
+        mongoClient.connect(url, function (err, client) {
+            let collections = client.db('pstracker').collection('peoples');
+            let bulk = collections.initializeUnorderedBulkOp();
+
+            documents.forEach(function (document) {
+                bulk.find(document.findO).upsert().updateOne(document.update);
+            });
+
+            bulk.execute(function (err, result) {
+                cb()
+            })
+        });
     }
 
 }
