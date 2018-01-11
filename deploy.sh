af13bbd3edb9674efb4500780d71b928d9b908ad
royh <royh@affilomania.com>
2018-01-11 13:56:03 +0200

    te

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,3 @@ docker build -t 127.0.0.1:5000/pstracker-pusher -f src/pusher-docker ./src
 docker push 127.0.0.1:5000/pstracker-nginx
 docker push 127.0.0.1:5000/pstracker
 docker push 127.0.0.1:5000/pstracker-pusher
-
